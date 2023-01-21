a824ba8f3e113d0246100df12f8e961e70073b62
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-21 21:32:53 +0300

    added notes, added nodes in README.md

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Let's build GPT: from scratch, in code, spelled out.
 Link to source material: https://youtu.be/kCc8FmEb1nY
 GitHub: https://github.com/karpathy/ng-video-lecture
+
+![Nodes](https://www.techiedelight.com/wp-content/uploads/Eulerian-path-for-directed-graphs.png "nodes")
--- math_trick.py
+++ math_trick.py
@@ -70,3 +70,35 @@ out = wei @ v
 # out = wei @ x
 
 print(out.shape)
+print(wei[0])
+
+# Notes:
+
+# Attention is a communication mechanism.
+# Can be seen as nodes in a directed graph looking at each other and aggregating
+# information with a weighted sum from all nodes that point to them,
+# with data-dependent weights.
+
+# There is no notion of space.
+# Attention simply acts over a set of vectors.
+# This is why we need to positionally encode tokens.
+
+# Each example across batch dimension is of course processed completely
+# independently and never "talk" to each other.
+
+# In an "encoder" attention block just delete the single line
+# that does masking with tril, allowing all tokens to communicate.
+# This block here is called a "decoder" attention block
+# because it has triangular masking, and is usually used in autoregressive settings,
+# like language modeling.
+
+# "Self-attention" just means that the keys and values are produced
+# from the same source as queries.
+# In "cross-attention", the queries still get produced from x,
+# but the keys and values come from some other,
+# external source (e.g. an encoder module)
+
+# "Scaled" attention additional divides wei by 1/sqrt(head_size).
+# This makes it so when input Q,K are unit variance,
+# wei will be unit variance too and Softmax will stay diffuse and not saturate too much.
+# Illustration below. (look at README.md)
