2954c22160fc7a9cb8c3474721115983334465e4
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-21 22:37:24 +0300

    write to file

--- gpt.py
+++ gpt.py
@@ -218,4 +218,5 @@ for iter in range(max_iters):
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-# open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+with open('more.txt', 'w') as f:
+    f.write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
